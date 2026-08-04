f4981605fd7a571aea28010c4699b7261fa30f80
Rafael Moreira Rosa <rafaelmoreira2921@gmail.com>
2026-08-04 18:07:16 -0300

    feat: Trabalhando no recebimento via Query String

---
+++ src/app/(admin)/tracks/[trackid]/page.tsx
@@ -0,0 +1,38 @@
+/*interface Props {
+  params: {
+    trackid: string;
+  }
+}
+export default async function Page({params} : Props) {
+  const p = await params
+  return (
+      <h1>Musica id {p.trackid}</h1>
+  );
+}*/
+interface Props {
+  params: {
+    trackid: string;
+  },
+  searchParams: {
+    mode: string,
+    showTitle: string,
+    anotherParam: string
+  }
+}
+
+export default async function Page({ params, searchParams }: Props) {
+  const p = await params
+  const trackid = p.trackid
+  const sP = await searchParams
+  const mode = sP.mode;
+  const showTitle = sP.showTitle;
+  const anotherParam = sP.anotherParam;
+  return (
+    <>
+      <h1>Musica ID: {trackid}</h1>
+      <h2>Mode: {mode}</h2>
+      <h2>Show: Title: {showTitle}</h2>
+      <h2>Another Param: {anotherParam}</h2>
+    </>
+  );
+}
--- src/app/admin/tracks/[trackid]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      <h1>Musica id 123</h1>
-    </div>
-  );
-}
